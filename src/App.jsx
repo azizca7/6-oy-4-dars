0ebb56c3eedb27bd68596577c2f5a3f748947a0b
azizca7 <ikromovazizbek256@gmail.com>
2025-01-30 17:40:24 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import Task5 from "./components/Task5";
 function App() {
   return (
     <>
-      {/* <Task1/> */}
-      {/* <Task2/> */}
-      {/* <Task3/> */}
-      {/* <Task4/> */}
-      {/* <Task5/> */}
+      <Task1/>
+      <Task2/>
+      <Task3/>
+      <Task4/>
+      <Task5/>
     </>
   );
 }
